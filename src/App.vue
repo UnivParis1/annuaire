ccddb3837ddd2d418ff0ac8df827f4f83d6a30a0
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2017-07-12 21:53:11 +0200

    always display List/Trombi/OrgChart

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,22 @@
             </ul>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-12" id="menus">
+            <span class="btn-group pull-right nav">
+              <router-link class="btn btn-primary" :class="{ youarehere: !query.format }" title="Afficher la liste" :to="withParam('format', undefined)">
+                  <span class='glyphicon glyphicon-th-list'></span> Liste
+              </router-link>
+              <router-link class="btn btn-primary" :class="{ youarehere: query.format === 'trombi' }" title="Afficher le trombinoscope" :to="withParam('format', 'trombi')" >
+                  <span class='glyphicon glyphicon-user'></span> Trombinoscope
+              </router-link>
+              <router-link class="btn btn-primary" :class="{ youarehere: query.format === 'chart' }" title="Afficher l'organigramme" :to="withParam('format', 'chart')" >
+                  <span class='glyphicon glyphicon-indent-left'></span> Organigramme
+              </router-link>
+            </span>
+        </div>
+    </div>    
   </div>
   <router-view></router-view>
 </div>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -7,17 +7,6 @@
         <div class="col-md-12" id="menus">
             <div class="bg-info" style="padding: 6px">
                 <span v-if="persons">{{persons.length}} résultat(s)</span>
-            <span class="btn-group pull-right nav">
-              <router-link class="btn btn-primary" :class="{ youarehere: !query.format }" title="Afficher la liste" :to="withParam('format', undefined)">
-                  <span class='glyphicon glyphicon-th-list'></span> Liste
-              </router-link>
-              <router-link class="btn btn-primary" :class="{ youarehere: query.format === 'trombi' }" title="Afficher le trombinoscope" :to="withParam('format', 'trombi')" >
-                  <span class='glyphicon glyphicon-user'></span> Trombinoscope
-              </router-link>
-              <router-link class="btn btn-primary" :class="{ youarehere: query.format === 'chart' }" title="Afficher l'organigramme" :to="withParam('format', 'chart')" >
-                  <span class='glyphicon glyphicon-indent-left'></span> Organigramme
-              </router-link>
-            </span>
            </div>
         </div>
       </div>
