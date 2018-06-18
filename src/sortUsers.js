de703b20792ceef1637ab9d8a8d7e422d0a7fe13
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2018-06-18 16:27:53 +0200

    use supannActivite-all to display & sort activitesUP1 (instead of using "description")
    https://projets.univ-paris1.fr/project/22/task/135

--- src/sortUsers.js
+++ src/sortUsers.js
@@ -8,7 +8,6 @@ function array_to_map2index(array) {
 }
 
 const employeeType2index = array_to_map2index(config.employeeTypes);
-const activitesUP1_to_index = array_to_map2index(config.activitesUP1);
 
 function number2fixedString(n) {
     const s = "" + n;
@@ -23,6 +22,10 @@ export const isPedagogyAffectation = (person) => (
     !!(person["supannEntiteAffectation-all"] || []).find(isPedagogy)
 );
 
+export const isActiviteUP1 = (activite) => (
+  activite.key.match(/^\{UAI:0751717J:ACT\}/)
+);
+
 export function descrAndWeight(person, isPedagogy, current_affectation_filter) {
     let weight;
 
@@ -33,19 +36,11 @@ export function descrAndWeight(person, isPedagogy, current_affectation_filter) {
       if (weight_) weight = "0_" + weight_;
     }
 
-    if (person.description) {
-      const weight_ = Math.min(... person.description.map(e => activitesUP1_to_index[e] || 9999));
-      // keep it only if it is listed in config.activitesUP1
-      if (weight_ !== 9999) return {
-        simplifiedDescription: person.description.join(', '),
-        weight: weight || ("2_" + number2fixedString(weight_)),
-      };
-    }
     if (person['supannActivite-all']) {
       const descr = person['supannActivite-all'].filter(activite => isPedagogy || !activite.key.match(/^\{CNU\}/)).map(activite => activite.name).join(', ')
       if (descr) return {
         simplifiedDescription: descr,
-        weight: weight || ("3_" + helpers.minString(person['supannActivite-all'].map(activite => activite.key))),
+        weight: weight || ("3_" + helpers.minString(person['supannActivite-all'].map(activite => (isActiviteUP1(activite) ? '0_' : '1_') + activite.key))),
       };
     }
     if (person.employeeType) {
